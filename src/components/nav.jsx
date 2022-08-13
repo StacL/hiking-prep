da4d3f3d23bb8d3257457a8005e059ad56103be0
Stacey_Lai <staceyclai@gmail.com>
2022-08-12 23:24:10 -0700

    project setup

---
+++ src/components/nav.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../styles/nav.css"
+
+export default function nav() {
+  return (
+    <div>nav</div>
+  )
+}
---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../styles/login.css"
+
+export default function LogIn() {
+  return (
+    <div>LogIn</div>
+  )
+}
